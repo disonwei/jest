f6d0fe0206a00dced4f7dea45a0e74d1cfb54cc8
dison <709142365@qq.com>
2021-07-29 21:28:04 +0800

    lesson3

--- math.test.js
+++ math.test.js
@@ -1,12 +1,26 @@
-const math = require("./math.js")
-
-const {
+import {
     add,
     minus,
-    mutil
-} = math;
+    multi
+} from "./math"
 
 
 test("测试加法", () => {
     expect(add(3, 7)).toBe(10);
 })
+
+test("测试减法", () => {
+    expect(minus(3, 7)).toBe(-4);
+})
+
+test("测试乘法", () => {
+    expect(multi(3, 7)).toBe(21);
+})
+
+
+// npm run jest
+// jest (babel-jest)
+// babel-core
+// 取 .babelre 配置
+// 在运行测试之前 结合babel 先把代码转化
+// 运行转化过的测试用例代码
